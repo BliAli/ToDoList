ab4a9f0ee5d764683a656cb526c2537d314490c4
BliAli <aliaddamawi@gmail.com>
2025-04-12 17:33:54 +0700

    Membuat kerangka awal appBar serta body di scaffold

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List toDoList = [
+    ['Learn Flutter', false],
+    ['Create Project', false],
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade300,
+      appBar: AppBar(
+        title: const Text('Simple ToDo'),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+              left: 20,
+              right: 20,
+              bottom: 0,
+            ),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Text(
+                toDoList[index][0],
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
